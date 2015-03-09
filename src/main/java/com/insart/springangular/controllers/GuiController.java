f757a88aa336af0e7d573062d3e4a03d80a58bf8
Evgeniy James <evgeniyjames@gmail.com>
2015-03-09 14:59:15 +0200

    Hello world page by /index

--- src/main/java/com/insart/springangular/controllers/GuiController.java
+++ src/main/java/com/insart/springangular/controllers/GuiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class GuiController {
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public ModelAndView index() {
         return new ModelAndView("index");
     }
